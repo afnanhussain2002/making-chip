e8626d6c8deab80b43d4b0aa252f68b6bfddcaac
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-27 22:13:35 +0600

    write a api for get a single video

--- src/app/api/get-video/[id]/route.ts
+++ src/app/api/get-video/[id]/route.ts
@@ -4,10 +4,22 @@ import { NextResponse, NextRequest } from "next/server";
 
 const prisma = new PrismaClient()
 
-export async function GET (request:NextRequest){
+export async function GET (request:NextRequest, { params }: { params: { id: string } }){
     try {
-        
+        const video = await prisma.tepmVideo.findUnique({
+            where:{
+                id:params.id
+            }
+            
+        })
+        if (!video) {
+           return NextResponse.json({message:"Video not found"}, {status:401})
+        }
+        return NextResponse.json(video,{status:200})
     } catch (error) {
-        
+        console.log(error);
+        return NextResponse.json({error:"Error on fetching video"}, {status:501})
+    }finally{
+        await prisma.$disconnect()
     }
 }
