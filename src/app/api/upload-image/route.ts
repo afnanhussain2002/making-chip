673912c4e36fb6604dc409328424951fae6eb62a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-28 10:42:21 +0600

    define a interface for cloudinary image result

--- src/app/api/upload-image/route.ts
+++ src/app/api/upload-image/route.ts
@@ -1,3 +1,15 @@
 import cloudinary from "cloudinary";
 import { NextRequest, NextResponse } from "next/server";
 
+
+interface CloudinaryImageResult{
+    public_id:string
+    originalSize:string
+    compressedSize:string
+
+}
+
+
+export async function POST(request:NextRequest) {
+    
+}
