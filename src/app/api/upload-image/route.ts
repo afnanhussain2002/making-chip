cea2c5b3cc8b073adeda3ac7212698630c186a66
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-28 11:09:32 +0600

    write code for optimize image

--- src/app/api/upload-image/route.ts
+++ src/app/api/upload-image/route.ts
@@ -1,34 +1,47 @@
 import { PrismaClient } from "@prisma/client";
 import cloudinary from "@/utils/cloudinary";
 import { NextRequest, NextResponse } from "next/server";
+import { UploadApiResponse } from "cloudinary";
 
 const prisma = new PrismaClient();
 
-interface CloudinaryImageResult{
-    public_id:string
-    originalSize:string
-    compressedSize:string
 
-}
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+    const file = formData.get("file") as string;
+    const originalSize = formData.get("originalSize") as string;
 
-
-export async function POST(request:NextRequest) {
-    try {
-        const formData = await request.formData();
-        const file = formData.get("file") as File;
-        const originalSize = formData.get("originalSize") as string;
-
-        if (!file) {
-            return NextResponse.json({message:"File not found"}, {status:401})
-        }
-
-        const result = await new Promise<CloudinaryImageResult>(resolve,reject) =>{
-            const uploadFile = await cloudinary.uploader.upload(file, {
-                resource_type:"auto",
-                folder:"making-chip-images",
-            })
-        }
-    } catch (error) {
-        
+    if (!file) {
+      return NextResponse.json({ message: "File not found" }, { status: 401 });
     }
+
+    const result = await new Promise<UploadApiResponse>(
+      (resolve, reject) => {
+        cloudinary.uploader.upload(
+          file,
+          {
+            resource_type: "auto",
+            folder: "making-chip-images",
+            transformation: [{ quality: "auto", fetch_format: "auto" }],
+          },
+          (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result as UploadApiResponse);
+            }
+          }
+        );
+      }
+    );
+
+    // Handle the result as needed
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong when upload the file" },
+      { status: 500 }
+    );
+  }
 }
