ae6a26eea274d1a2061a32e9748b3f04fb8b8131
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-28 12:53:37 +0600

    fix some issues

--- src/app/api/upload-image/route.ts
+++ src/app/api/upload-image/route.ts
@@ -5,40 +5,50 @@ import { UploadApiResponse } from "cloudinary";
 
 const prisma = new PrismaClient();
 
+interface CloudinaryImageUpload {
+  public_id: string;
+  [key: string]: any;
+}
 
 export async function POST(request: NextRequest) {
   try {
     const formData = await request.formData();
-    const file = formData.get("file") as string;
+    const file = formData.get("file") as File;
     const originalSize = formData.get("originalSize") as string;
+    console.log("File-------", file, originalSize);
 
     if (!file) {
       return NextResponse.json({ message: "File not found" }, { status: 401 });
     }
 
-    const result = await new Promise<UploadApiResponse>(
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const result = await new Promise<CloudinaryImageUpload>(
       (resolve, reject) => {
-        cloudinary.uploader.upload(
-          file,
+        const uploadStream = cloudinary.uploader.upload_stream(
           {
             resource_type: "auto",
             folder: "making-chip-images",
             transformation: [{ quality: "auto", fetch_format: "auto" }],
           },
-          (error, result) => {
+          (error, uploadResult) => {
             if (error) {
               reject(error);
+              console.log("Error-------", error);
             } else {
-              resolve(result as UploadApiResponse);
+              resolve(uploadResult as CloudinaryImageUpload);
             }
           }
         );
+        uploadStream.end(buffer);
       }
     );
-
+    // console.log("Result-------", result);
     // Handle the result as needed
     return NextResponse.json({ ...result, originalSize }, { status: 200 });
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       { message: "Something went wrong when upload the file" },
       { status: 500 }
