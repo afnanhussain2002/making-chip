9f88fde4f7d2a33651aa06ca13d9d6ef7d12b9bd
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-30 10:06:38 +0600

    write some state

--- src/app/(app)/image-restore/page.tsx
+++ src/app/(app)/image-restore/page.tsx
@@ -4,6 +4,11 @@ import axios from 'axios'
 import {CldImage} from 'next-cloudinary';
 
 function ImageRestore() {
+    const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+    const [isUploading, setIsUploading] = useState(false);
+    const [imageWidth, setImageWidth] = useState<number>(0);
+    const [imageHeight, setImageHeight] = useState<number>(0);
+    const imageRef = useRef<HTMLImageElement>(null);
   return (
     <div>ImageRestore</div>
   )
