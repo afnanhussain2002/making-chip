8389f49065fe172caeeb65f8e459828ffe2c7f6b
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-30 09:57:53 +0600

    fix the issue and complete the image clean route

--- src/app/api/image-restore/route.ts
+++ src/app/api/image-restore/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 // const prisma = new PrismaClient();
 
-interface CloudinaryImageOptimize {
+interface CloudinaryImageRestore {
   public_id: string;
   [key: string]: string | number | boolean | null;
 }
@@ -15,7 +15,8 @@ export async function POST(request: NextRequest) {
     const formData = await request.formData();
     const file = formData.get("file") as File;
     const originalSize = formData.get("originalSize") as string;
-    // console.log("File-------", file, originalSize);
+    console.log("File-------", file.name,);
+
 
     if (!file) {
       return NextResponse.json({ message: "File not found" }, { status: 401 });
@@ -24,20 +25,19 @@ export async function POST(request: NextRequest) {
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
 
-    const result = await new Promise<CloudinaryImageOptimize>(
+    const result = await new Promise<CloudinaryImageRestore>(
       (resolve, reject) => {
         const uploadStream = cloudinary.uploader.upload_stream(
           {
-            resource_type: "auto",
+            resource_type: "image",
             folder: "making-chip-images",
-            transformation: [{ quality: "auto", fetch_format: "auto" }],
           },
           (error, uploadResult) => {
             if (error) {
               reject(error);
               console.log("Error-------", error);
             } else {
-              resolve(uploadResult as CloudinaryImageOptimize);
+              resolve(uploadResult as CloudinaryImageRestore);
             }
           }
         );
@@ -45,8 +45,15 @@ export async function POST(request: NextRequest) {
       }
     );
     // console.log("Result-------", result);
+    const restoreImage = cloudinary.url(result.public_id,{
+                   
+        transformation: [
+            { effect: 'gen_restore' }   // Sharpening effect (adjust value as needed)
+          ]
+    
+    })
     // Handle the result as needed
-    return NextResponse.json({ ...result, originalSize }, { status: 200 });
+    return NextResponse.json({ restoreImage}, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json(
