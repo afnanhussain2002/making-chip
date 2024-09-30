a6207559308eca9bc624b2bb8f943429c1722001
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-30 10:09:15 +0600

    write the full func for handle file upload

--- src/app/(app)/image-restore/page.tsx
+++ src/app/(app)/image-restore/page.tsx
@@ -11,7 +11,30 @@ function ImageRestore() {
     const imageRef = useRef<HTMLImageElement>(null);
 
     const handleFileUpload = async(e: React.ChangeEvent<HTMLInputElement>) => {
-        
+        e.preventDefault();
+        const file = e.target.files?.[0];
+        if (!file) return;
+        setIsUploading(true);
+        const formData = new FormData();
+        formData.append("file", file);
+        try {
+          const response = await axios.post("/api/restore-image", formData);
+          console.log(response);
+          if (!response.data) {
+            throw new Error("Failed to upload image");
+          }
+          const data = response.data
+          console.log("width", data.width, "height",data.height);
+          setUploadedImage(data.public_id);
+         setImageWidth(data.width)
+         setImageHeight(data.height)
+          return;
+        } catch (error) {
+          console.log(error);
+         return alert("Failed to upload image");
+        }finally{
+          setIsUploading(false);
+        }
     }
   return (
     <div>ImageRestore</div>
