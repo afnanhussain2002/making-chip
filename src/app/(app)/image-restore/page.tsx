bdb21b0c67e5d0e1f1194ed028c753aac3765b71
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-30 10:15:34 +0600

    write full ui for restore image

--- src/app/(app)/image-restore/page.tsx
+++ src/app/(app)/image-restore/page.tsx
@@ -1,44 +1,115 @@
-"use client"
-import React,{useState,useRef} from 'react'
-import axios from 'axios'
-import {CldImage} from 'next-cloudinary';
+"use client";
+import React, { useRef, useState } from "react";
+import axios from "axios";
+import {CldImage} from "next-cloudinary";
 
-function ImageRestore() {
-    const [uploadedImage, setUploadedImage] = useState<string | null>(null);
-    const [isUploading, setIsUploading] = useState(false);
-    const [imageWidth, setImageWidth] = useState<number>(0);
-    const [imageHeight, setImageHeight] = useState<number>(0);
-    const imageRef = useRef<HTMLImageElement>(null);
+function ImageOptimize() {
 
-    const handleFileUpload = async(e: React.ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        const file = e.target.files?.[0];
-        if (!file) return;
-        setIsUploading(true);
-        const formData = new FormData();
-        formData.append("file", file);
-        try {
-          const response = await axios.post("/api/restore-image", formData);
-          console.log(response);
-          if (!response.data) {
-            throw new Error("Failed to upload image");
-          }
-          const data = response.data
-          console.log("width", data.width, "height",data.height);
-          setUploadedImage(data.public_id);
-         setImageWidth(data.width)
-         setImageHeight(data.height)
-          return;
-        } catch (error) {
-          console.log(error);
-         return alert("Failed to upload image");
-        }finally{
-          setIsUploading(false);
-        }
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [imageWidth, setImageWidth] = useState<number>(0);
+  const [imageHeight, setImageHeight] = useState<number>(0);
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  const handleFileUpload = async(e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const file = e.target.files?.[0];
+    if (!file) return;
+    setIsUploading(true);
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      const response = await axios.post("/api/optimize-image", formData);
+      console.log(response);
+      if (!response.data) {
+        throw new Error("Failed to upload image");
+      }
+      const data = response.data
+      console.log("width", data.width, "height",data.height);
+      setUploadedImage(data.public_id);
+     setImageWidth(data.width)
+     setImageHeight(data.height)
+      return;
+    } catch (error) {
+      console.log(error);
+     return alert("Failed to upload image");
+    }finally{
+      setIsUploading(false);
     }
+  }
+
+  /* const handleImageLoad = () => {
+    if (imageRef.current) {
+      setImageWidth(imageRef.current.naturalWidth);
+      setImageHeight(imageRef.current.naturalHeight)
+      return
+    }
+  } */
+
+    const handleDownload = () =>{
+      if (!imageRef.current) return;
+      fetch(imageRef.current.src)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const url = URL.createObjectURL(blob);
+          const link = document.createElement("a");
+          link.href = url;
+          link.download = `${uploadedImage?.replace(/\s+/g, "_").toLowerCase()}.png`;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+         window.URL.revokeObjectURL(url);
+        })
+        .catch((error) => {
+          console.error("Error downloading image:", error);
+        });
+    } 
+
   return (
-    <div>ImageRestore</div>
-  )
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Image Restore!</h1>
+          <p className="py-6">
+            Restore any image to its original state.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl border border-primary">
+          {isUploading ? <div className="loading loading-lg"></div>
+        :
+        <>
+        {
+          uploadedImage ? <>
+          <CldImage
+            ref={imageRef}
+            width={imageWidth}
+            height={imageHeight}
+            src={uploadedImage}
+            className="w-full"
+            alt="Uploaded Image"
+          />
+          <button onClick={handleDownload} className="btn btn-primary">Download</button>
+          </>
+          :
+          <form className="card-body">
+          <input
+            type="file"
+            className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+            onChange={handleFileUpload}
+          />
+          {/* <div className="form-control mt-6">
+            <button className="btn btn-primary">Optimize</button>
+          </div> */}
+        </form>
+        }
+        
+        </>
+
+        }
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ImageRestore
+export default ImageOptimize;
