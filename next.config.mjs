9ecba401859feea5538d35ec24e2f464b0e4807c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-03 09:34:15 +0600

    change the main route path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/home', // The route you want to redirect to
+          permanent: true, // Set to true for a 301 redirect, or false for a temporary redirect
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
