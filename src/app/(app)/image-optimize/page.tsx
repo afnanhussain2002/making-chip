d5e53aaa25bec403ac6a3cea4028bbfd1b8c7f77
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-03 09:37:19 +0600

    change the bg color of that page

--- src/app/(app)/image-optimize/page.tsx
+++ src/app/(app)/image-optimize/page.tsx
@@ -66,7 +66,7 @@ function ImageOptimize() {
     } 
 
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Image Optimization!</h1>
