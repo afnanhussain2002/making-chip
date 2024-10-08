176b089bcdee46ca7e79d4dc1e109739e649e537
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-08 11:11:07 +0600

    fix responsive issues

--- src/components/FunnyTemplates.tsx
+++ src/components/FunnyTemplates.tsx
@@ -61,94 +61,83 @@ function FunnyTemplates() {
 
   return (
     <div>
-      <h3 className="text-3xl text-center font-bold">Funny Templates</h3>
-
-      <div className="flex justify-center items-center mt-5">
-        <div id="template" className="w-[500px] h-[500px] bg-customRed">
-          <div className="border-4 border-customRed">
-            <Image
-              src={originalImage ? originalImage : motuImage}
-              alt="image"
-              width={300}
-              height={300}
-              className="w-full h-80 object-cover object-center"
-              onLoadingComplete={() => setImagesLoaded(true)}
-            ></Image>
-          </div>
-          <div className="font-bold text-center m-3 text-xl">
-         <>
-      {words?.map((word, index) => (
-        <span key={index}>{word} </span>
-      ))}
-      {lastWord && (
-        <span style={{ color: 'yellow' }}>{lastWord}</span>
-      )}
-         
-         </>
-    </div>
-          <p className="font-bold text-right mt-6 mr-3 text-sm relative top-12">
-            {realDate.toDateString()}
-          </p>
-
-          <div className="flex text-xs items-center relative top-14 bg-customRed2 justify-center gap-1 p-2">
-            {/* <Image
-              src={jomunaState ? jomunaState : jomunaImage}
-              alt="image"
-              width={100}
-              height={50}
-              className="w-full "
-              onLoadingComplete={() => setImagesLoaded(true)}
-            ></Image> */}
-            <img
-              src="https://i.ibb.co.com/2sqWQSL/jomuna-logo.png"
-              alt="img"
-              width={100}
-              height={50}
-              className="w-40 "
-            />
-            <p className="flex items-center">
-              {" "}
-              <StepForward /> www.jomuna.tv
-            </p>
-            <p className="flex items-center">
-              <Facebook />
-              jomunatelevision
-            </p>
-            <p className="flex gap-1 items-center">
-              <Youtube />
-              jomunatvbd
-            </p>
-          </div>
+    <h3 className="text-3xl text-center font-bold">Funny Templates</h3>
+  
+    <div className="flex justify-center items-center mt-5">
+      <div id="template" className="w-full sm:w-[400px] md:w-[450px] lg:w-[500px] h-auto bg-customRed">
+        <div className="border-4 border-customRed">
+          <Image
+            src={originalImage ? originalImage : motuImage}
+            alt="image"
+            width={300}
+            height={300}
+            className="w-full h-80 object-cover object-center"
+            onLoadingComplete={() => setImagesLoaded(true)}
+          />
         </div>
-      </div>
-      <div className="text-center">
-      <button
-          onClick={handleDownload}
-          className="btn btn-primary mt-20"
-          disabled={isDownloading}
-        >
-          {isDownloading ? "Downloading..." : "Download as Image"}
-        </button>
+        <div className="font-bold text-center m-3 text-lg md:text-xl">
+          <>
+            {words?.map((word, index) => (
+              <span key={index}>{word} </span>
+            ))}
+            {lastWord && (
+              <span style={{ color: 'yellow' }}>{lastWord}</span>
+            )}
+          </>
+        </div>
+        <p className="font-bold text-right mt-6 mr-3 text-sm relative top-10">
+          {realDate.toDateString()}
+        </p>
+  
+        <div className="flex text-xs items-center relative top-10 bg-customRed2 justify-center gap-1 sm:gap-1 p-2">
+  <img
+    src="https://i.ibb.co.com/2sqWQSL/jomuna-logo.png"
+    alt="img"
+    className="w-32"
+  />
+  <div className="flex sm:gap-1">
+    <p className="flex items-center">
+      <StepForward /> www.jomuna.tv
+    </p>
+    <p className="flex items-center">
+      <Facebook /> jomunatelevision
+    </p>
+    <p className="flex items-center">
+      <Youtube /> jomunatvbd
+    </p>
+  </div>
+</div>
 
-      </div>
-      <div className="mt-20">
-        <form className="flex flex-col justify-center items-center gap-2 ">
-          <input
-            type="file"
-            className="file-input file-input-bordered file-input-primary w-full max-w-xs"
-            onChange={handleFileUpload}
-          />
-
-          <input
-            type="text"
-            onChange={handleTextChange}
-            placeholder="Enter text"
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-        </form>
-        
       </div>
     </div>
+  
+    <div className="text-center">
+      <button
+        onClick={handleDownload}
+        className="btn btn-primary mt-20"
+        disabled={isDownloading}
+      >
+        {isDownloading ? "Downloading..." : "Download as Image"}
+      </button>
+    </div>
+  
+    <div className="mt-20">
+      <form className="flex flex-col justify-center items-center gap-2 ">
+        <input
+          type="file"
+          className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+          onChange={handleFileUpload}
+        />
+        <input
+          type="text"
+          onChange={handleTextChange}
+          placeholder="Enter text"
+          className="input input-bordered input-primary w-full max-w-xs"
+        />
+      </form>
+    </div>
+  </div>
+  
   );
 }
 
