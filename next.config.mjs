9dbac16bebd239379417035105eb23b532104316
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-08 10:43:47 +0600

    add some domains on that file

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/home', // The route you want to redirect to
-          permanent: true, // Set to true for a 301 redirect, or false for a temporary redirect
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['res.cloudinary.com','i.ibb.co.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
