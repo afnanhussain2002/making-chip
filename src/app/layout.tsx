4c0ff80745df10439b047c58a81bc0e5a0a40d89
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-13 21:11:21 +0600

    add google tag manager no script code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Script from "next/script";
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
-
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,15 +19,17 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.makingchip.com"),
   title: "Making Chip - Ai Image Optimize",
-  description: "You can optimize your image, fill image background using AI, and much more.",
-  openGraph:{
+  description:
+    "You can optimize your image, fill image background using AI, and much more.",
+  openGraph: {
     title: "Making Chip",
-    description: "You can optimize your image, fill image background using AI, and much more.",
+    description:
+      "You can optimize your image, fill image background using AI, and much more.",
     url: "https://www.makingchip.com",
-    type:"website",
-    locale:"en-US",
-    siteName:"Making Chip",
-  }
+    type: "website",
+    locale: "en-US",
+    siteName: "Making Chip",
+  },
 };
 
 export default function RootLayout({
@@ -37,23 +38,41 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <ClerkProvider>
-     <html lang="en" >
-     <head>
-    <meta name="google-site-verification" content="O5p1N30N-if12YTterX8Ozmw3693-QCw8gCjefcTVIY" />
-    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8402742608812827"
-     crossOrigin="anonymous"></Script>
-     <GoogleAnalytics gaId="G-FFJ0T2MWJS"/>
-<GoogleTagManager gtmId="GTM-M5QG48T6"/>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <meta
+            name="google-site-verification"
+            content="O5p1N30N-if12YTterX8Ozmw3693-QCw8gCjefcTVIY"
+          />
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8402742608812827"
+            crossOrigin="anonymous"
+          ></Script>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-FFJ0T2MWJS"
+          ></Script>
+          <GoogleAnalytics gaId="G-FFJ0T2MWJS" />
+          <GoogleTagManager gtmId="GTM-M5QG48T6" />
+        </head>
+        <body
+          data-theme={"synthwave"}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M5QG48T6"
+              height="0"
+              width="0"
+              style={{display:"none",visibility:"hidden"}}
+            ></iframe>
+          </noscript>
 
-    </head>
-      <body
-      data-theme={"synthwave"}
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-   </ClerkProvider>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
