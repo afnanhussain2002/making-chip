7890e15aeb1aef783126118ae86c5e3adf62d0db
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-20 20:09:14 +0600

    add dummy user image

---
+++ src/app/(app)/post-template/page.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react'; 
+import { Metadata } from 'next';
+import PostTemplate from '@/components/PostTemplate';
+
+
+export const metadata: Metadata = {
+  title: "Post Template | Making Chip - Ai Image Optimize",
+  description: "A template is a pre-made design that can be used to create an image. ",
+  keywords: ["imagePost", "Image Post", "Post Template", "Making Chip"],
+
+};
+
+const PostTemplatePage = () => {
+  return (
+    <div>
+   <PostTemplate />
+    </div>
+  );
+};
+
+export default PostTemplatePage;
