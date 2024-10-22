7194d87e7e230d6d0326170355bca39f660c6222
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-22 10:14:50 +0600

    add code for page index

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com','i.ibb.co.com'],
+    domains: ['res.cloudinary.com', 'i.ibb.co.com'],
+  },
+  async headers() {
+    return [
+      {
+        // This example sets headers for the /sitemap.xml path
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow", // Allow indexing and following links
+          },
+        ],
+      },
+    ];
   },
 };
 
