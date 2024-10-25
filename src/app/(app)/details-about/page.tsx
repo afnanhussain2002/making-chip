432d59fad518189e844008f1b99a2ea2fc4445f8
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-25 20:43:34 +0600

    add that blog

---
+++ src/app/(app)/details-about/page.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import MakingChip from '@/app/opengraph-image.png';
+import Image from "next/image";
+import Link from "next/link";
+
+const BlogPost = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <main className="mt-10">
+        <div className="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative" style={{ height: "24em" }}>
+          <div
+            className="absolute left-0 bottom-0 w-full h-full z-10"
+            style={{ backgroundImage: "linear-gradient(180deg,transparent,rgba(0,0,0,.7))" }}
+          ></div>
+          <Image
+            src={MakingChip}
+            className="absolute left-0 top-0 w-full h-full z-0 object-cover"
+            alt="Blog background"
+          />
+          <div className="p-4 absolute bottom-0 left-0 z-20">
+            <h2 className="text-4xl font-semibold text-gray-100 leading-tight">
+              Boost Your Website&apos;s Performance with Image Optimization
+            </h2>
+           
+          </div>
+        </div>
+
+        <div className="px-4 lg:px-0 mt-12 text-white max-w-screen-md mx-auto text-lg leading-relaxed">
+          <p className="pb-6">
+            Image optimization enhances web performance, user experience, and SEO. Here&apos;s a breakdown of why it&apos;s so important in each of these areas:
+          </p>
+
+          <h2 className="text-2xl text-primary font-semibold mb-4">Web Performance</h2>
+          <p className="pb-6">
+            <strong className="text-primary">Faster page load times:</strong> Images are often the largest assets on a webpage, and large, unoptimized images can slow down page loading. Optimized images load faster, reducing the time it takes for a webpage to fully render. Faster-loading pages keep users engaged and reduce the chance of them abandoning your site.
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">Reduced Bandwidth Usage:</strong> Optimized images consume less bandwidth, which is beneficial for both website owners and users. This is particularly crucial for mobile users and those with limited data plans, as it reduces the amount of data needed to view the page.
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">Improved Server Performance:</strong> When images are optimized, server load decreases because smaller file sizes mean less data to process and deliver. This results in faster server response times and better overall performance, especially for sites with high traffic.
+          </p>
+
+          <h2 className="text-2xl text-primary font-semibold mb-4">User Experience</h2>
+          <p className="pb-6">
+            <strong className="text-primary">Enhanced Visual Appeal:</strong> Image optimization retains visual quality while reducing file size. This ensures that images look sharp and professional without compromising load speed, enhancing the visual appeal of the website.
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">Reduced Bounce Rate:</strong> A slow website can frustrate users, leading to high bounce rates (users leaving the site without interacting). By optimizing images and improving load speed, you provide a seamless experience that keeps users engaged.
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">Accessibility Across Devices:</strong> Optimized images are more likely to display consistently across different devices and screen sizes. This ensures that mobile users, in particular, have a positive experience, which is essential given the increasing use of smartphones for browsing.
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">Smoother Interactions:</strong> Faster-loading images contribute to smoother interactions, such as quick navigation and scrolling, which enhances overall usability. Users are more likely to stay on a site that feels responsive and polished.
+          </p>
+
+          <h2 className="text-2xl text-primary font-semibold mb-4">Search Engine Optimization (SEO)</h2>
+          <p className="pb-6">
+            <strong className="text-primary">Higher Search Rankings:</strong> Google and other search engines use page speed as a ranking factor. Optimized images contribute to faster page load times, which can improve a site&apos;s ranking in search engine results pages (SERPs).
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">Image SEO Opportunities:</strong> Optimized images can have descriptive filenames, alt attributes, and captions, which search engines can index. This enhances the chances of images appearing in image search results, driving additional traffic to your site.
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">Lower Bounce Rate Benefits SEO:</strong> A fast-loading website can lead to a lower bounce rate, signaling to search engines that your site is valuable and relevant. This can positively impact your rankings.
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">Better Core Web Vitals:</strong> Google uses Core Web Vitals, which are a set of metrics related to page speed and user interaction, as a ranking factor. Optimized images improve metrics like Largest Contentful Paint (LCP), directly contributing to better SEO performance.
+          </p>
+
+          <h2 className="text-2xl text-primary font-semibold mb-4">Why Use Our Tools</h2>
+          <p className="pb-6">
+            <strong className="text-primary">100% Free for All:</strong> Makingchip.com is 100% free for all users. You don’t need to pay a single cent. We may add some premium features in the future, but for now, everything is 100% free.
+          </p>
+          <p className="pb-6">
+            <strong>No sign-up needed:</strong> Anyone can use it without sign-in/up. And all of the features of makingchip.com, you can use without sign-in.
+          </p>
+          <p className="pb-6">
+            <strong className="text-primary">We don’t keep your data:</strong> When you optimize your image or use any of the features of makingchip.com, we don&apos;t keep your data. Your data will be automatically deleted after downloading your image.
+          </p>
+
+          <h2 className="text-2xl text-primary font-semibold mb-4">More Features of makingchip.com</h2>
+          <p className="pb-6">
+           <Link href={"/image-optimize"}><strong className="text-primary">Image Optimize:</strong> </Link>  Our tools will optimize your image up to 40% without losing the quality of the image.
+          </p>
+          <p className="pb-6">
+           <Link href={"/image-restore"}><strong className="text-primary">Image Quality Improvements:</strong></Link>  Our tools can improve your image quality a little bit. But now it is in beta version, and as soon as possible we will launch the full version.
+          </p>
+          <p className="pb-6">
+            <Link href={"/image-fill"}><strong className="text-primary">Image Background Fill:</strong></Link> Makingchip.com can fill any image background using AI. It performs better on single- or double-person images. For lots of people, the image is not ready to fill the background perfectly. It is currently in beta.
+          </p>
+          <p className="pb-6">
+           <Link href={"/image-formats"}> <strong className="text-primary">Image Social Media Formats:</strong></Link> You can upload any image and change its format for social platforms like Facebook, Instagram, and Twitter. Soon, we will add more social formats.
+          </p>
+          <p className="pb-6">
+            <Link href={"/funny-templates"}><strong className="text-primary">Funny Templates:</strong></Link> We have added funny templates for making humorous news. You just upload a picture and write your caption, and it adjusts the image and text to create funny news templates for sharing with friends.
+          </p>
+          <p className="pb-6">
+           <Link href={"/post-template"}> <strong className="text-primary">Post Templates:</strong></Link> Sometimes, we need to post something as an image on social media. Our post templates allow you to write posts, add your profile picture and name, and select post text colors easily.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default BlogPost;
