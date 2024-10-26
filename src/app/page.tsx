456d1a860c199a1ccc7daf9eaa069f8c2653ea72
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-26 08:26:27 +0600

    write home page code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,39 @@
+import Image from "next/image";
 import Link from "next/link";
+import HeroImage from "@/public/hero.png"
+import MainFeatures from "@/components/MainFeatures";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-    <div className="text-center p-6 shadow-md rounded-lg border border-primary">
-      <h1 className="text-2xl font-bold mb-4">Welcome To Making Chip</h1>
-      <p className="mb-4 w-96">You can optimize images, fill up the image background, And make some funny templates and much more.</p>
-      <Link href="/home" className="bg-primary font-bold text-white py-2 px-4 rounded">Get Started</Link>
+    <>
+    <div className="hero bg-base-200 min-h-[600px] border-b border-primary">
+    <div className="hero-content flex-col lg:flex-row-reverse">
+      <Image
+        src={HeroImage}
+        className=" rounded-lg shadow-2xl flex-1" 
+        width={500}
+        height={500}
+        alt="hero image"
+        />
+        
+      <div className="flex-1">
+        <h1 className="text-5xl font-bold text-primary">Play With Your Images!</h1>
+        <p className="py-6">
+        Boost Your Website&apos;s Performance with using our tools. You can optimize, format, and restore your images with just a few clicks.
+        </p>
+       <Link href={"/image-optimize"}>
+       <button className="btn btn-primary">Get Started</button>
+       </Link> 
+      </div>
     </div>
   </div>
+
+
+  <MainFeatures/>
+
+
+    
+    </>
   
   );
 }
