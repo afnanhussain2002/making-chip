3d5a281171a33509983b1cd053b50f9840981e80
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-10-27 15:15:48 +0600

    change some layout code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -120,7 +120,7 @@ export default function RootLayout({
       <li><Link href={"/home"}>Home</Link></li>
           <li><Link href={"/about"}>About</Link></li>
           <li><Link href={"/contact"}>Contact</Link></li>
-          <li><Link href={"/blog"}>Blog</Link></li>
+          <li><Link href={"/details-about"}>Blog</Link></li>
           <li><Link href={"/image-optimize"}>Image Optimize</Link></li>
           <li><Link href={"/image-fill"}>Image Bg Fill</Link></li>
          
